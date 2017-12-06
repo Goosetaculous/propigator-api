d13739eceae7da28b7142e65eae7248e5ec1ff71
Goose <josephtrop@gmail.com>
2017-12-05 20:49:12 -0800

    new branch

---
+++ controller/utility/zillow.js
@@ -0,0 +1,21 @@
+var Zillow  = require('node-zillow')
+
+var zwsid = "X1-ZWz1f5677kiadn_6bk45"
+var zillow = new Zillow(zwsid)
+
+var parameters = {
+ zpid: 1111111
+};
+
+
+
+
+ module.exports = {
+    tryZillow:(cb)=>{
+        zillow.get('GetZestimate', parameters)
+        .then(function(results) {
+            cb(results)
+          // results here is an object { message: {}, request: {}, response: {}}  
+        })
+    } 
+ }

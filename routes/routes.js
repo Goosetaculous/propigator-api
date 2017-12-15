d5961ef4dd12baaedf5673aafe8b790bcb7e58aa
abocado <abo@adboomgrp.com>
2017-12-15 08:05:38 -1000

    adds x-auth to the list of authorized header values

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ var api =  require('../controller/api/endpoints')
 module.exports=(app)=>{
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
         next();
     });
     app.get("/",(req,res,next)=>{

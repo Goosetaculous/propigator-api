fe805956c0d072d5df274119fc68ca121a93538c
abocado <abo@adboomgrp.com>
2017-12-14 19:03:12 -1000

    Adding cors to expressjs backend

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,12 @@
 var property = require("../controller/api/propery");
 
 module.exports=(app)=>{
-    app.get("/",(req,res)=>{
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+    app.get("/",(req,res,next)=>{
         res.status(200).send("test")
     })
     app.post("/property", property.get_property)
